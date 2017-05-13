b8572063fd627481d130ce96c838ba1b8e7ae8f2
Karol Musur <kamusur@gmail.com>
2017-05-13 23:42:15 +0200

    Save global state to localstorage

--- src/index.js
+++ src/index.js
@@ -1,14 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, compose } from 'redux';
+import persistState from 'redux-localstorage';
 
 import { countdown } from './reducers';
 import App from './App';
 
 import './index.css';
 
-const store = createStore(countdown);
+const enhancer = compose(
+  persistState(['endTime', 'name'], {
+    serialize: (collection) => {
+      return JSON.stringify(collection, (key, value) => {
+        if (!isNaN(Date.parse(value))) {
+          return `date:${value.toString()}`;
+        } else {
+          return value;
+        }
+      });
+    },
+    deserialize: (collection) => {
+      return JSON.parse(collection, (key, value) => {
+        if (typeof value === 'string' && value.includes('date:')) {
+          return new Date(value.slice(5, value.length));
+        } else {
+          return value;
+        }
+      });
+    }
+  }),
+);
+
+const store = createStore(countdown, enhancer);
 
 ReactDOM.render(
   <Provider store={store}>
